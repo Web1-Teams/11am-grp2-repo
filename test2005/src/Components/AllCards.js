2230eb270a3bab6705eb4eaed3928e52a5c1417d
shifaasuwan <s12323958@stu.najah.edu>
2024-12-31 20:06:28 +0200

    added files for components

---
+++ test2005/src/Components/AllCards.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card'
+const AllCards =() => {
+    //img= image,name=name of place,loc=location,site=web site 
+          return(
+            <div className='card-boxx'>
+                <Card
+                img ={''}
+                name= ""
+                loc=""
+                site=''
+                />
+                
+               
+            </div>
+          )
+   
+}
+
+export default AllCards
